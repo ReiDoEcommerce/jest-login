70fb9e8174ccf5fae8df733cace22ce8f2fd1d85
Lukita <lucmaieski@gmail.com>
2022-12-02 01:27:06 -0300

    Refact third test

--- __tests__/unit/components/Login/Login.test.tsx
+++ __tests__/unit/components/Login/Login.test.tsx
@@ -1,8 +1,16 @@
 import SignInComponent from "../../../../components/sections/login/signIn";
-import { act, fireEvent, render, screen } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  useEvent,
+} from "@testing-library/react";
 import { AuthProvider } from "../../../../src/contexts/Auth";
 import { ProfileProvider } from "src/contexts/Profile";
+import { api } from "../../../../src/services/api";
 import "@testing-library/jest-dom";
+import axios from "axios";
 
 describe("Login tests", () => {
   describe("Inputs validation", () => {
@@ -10,6 +18,10 @@ describe("Login tests", () => {
       const useRouter = jest.spyOn(require("next/router"), "useRouter");
       useRouter.mockImplementation(() => ({
         pathname: "/",
+      }));
+
+      jest.mock("../../../../src/services/api", () => ({
+        post: jest.fn(() => ({ data: { token: "23123" } })),
       }));
 
       render(
@@ -55,9 +67,50 @@ describe("Login tests", () => {
       expect(passwordMessageError).toBeInTheDocument();
     });
 
-    it("should not send a signIn request if at least one field is empty", async () => {});
+    it("should not send a signIn request if at least one field is empty", async () => {
+      const passwordInput: any = screen.getByPlaceholderText(/Password/i);
+      const signInButton = screen.getByTestId("button-submit-login");
 
-    it("should send a signIn request if all fields are valids", async () => {});
+      expect(passwordInput).toBeInTheDocument();
+      expect(signInButton).toBeInTheDocument();
+
+      expect(passwordInput.value).toHaveLength(0);
+
+      const spyAxiosRequest = jest.spyOn(api, "post");
+
+      await act(() => {
+        fireEvent.click(signInButton);
+        expect(spyAxiosRequest).toHaveBeenCalledTimes(0);
+      });
+    });
+
+    it("should send a signIn request if all fields are valids", async () => {
+      const emailInput: any = screen.getByPlaceholderText(/Email/i);
+      const passwordInput: any = screen.getByPlaceholderText(/Password/i);
+      const signInButton = screen.getByTestId("button-submit-login");
+
+      expect(passwordInput).toBeInTheDocument();
+      expect(emailInput).toBeInTheDocument();
+      expect(signInButton).toBeInTheDocument();
+
+      expect(passwordInput.value).toHaveLength(0);
+      expect(emailInput.value).toHaveLength(0);
+
+      await act(() => {
+        fireEvent.change(emailInput, {
+          target: { value: "Lucmaieski@gmail.com" },
+        });
+        fireEvent.change(passwordInput, { target: { value: "123" } });
+      });
+
+      expect(emailInput.value).not.toHaveLength(0);
+      expect(passwordInput.value).not.toHaveLength(0);
+
+      const spyAxiosRequest = jest.spyOn(api, "post");
+      await act(() => fireEvent.click(signInButton));
+
+      expect(spyAxiosRequest).toHaveBeenCalledTimes(1);
+    });
   });
 
   describe("Data validation", () => {
